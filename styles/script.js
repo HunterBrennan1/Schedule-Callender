ae539a50fcaf9175cac3e5579756f221388f48a6
Hunter <hunterbrennan15@gmail.com>
2022-07-18 20:37:03 -0400

    cant figure out the JS for this project so I will try again at a later date

--- styles/script.js
+++ styles/script.js
@@ -1,1 +1,18 @@
-console.log(moment());
+var today = moment();
+$("#currentDay").text(today.format("MMM Do, YYYY"));
+
+var currentHour = moment().format('LT');
+
+let plan = "saved";
+document.getElementById("textarea").innerHTML = plan;
+
+let btn = "save-button";
+document.getElementById("saveBtn").innerHTML = btn;
+
+const textarea = document.querySelector(textarea)
+function save() {
+  localStorage.setItem("text", textarea.value);
+}
+
+
+
